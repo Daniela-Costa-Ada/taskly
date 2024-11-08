4edddae2fcc01bfb46192bd47e012d799a296539
Daniela Costa <daniela.costa@sapientag2.com.br>
2024-11-08 08:46:10 -0300

    Initial commit

---
+++ app/Http/Controllers/API/TaskController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        $task = Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'is_completed' => false,
+        ]);
+
+        return response()->json(['task' => $task], 201);
+    }
+}
